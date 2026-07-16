715d0f0d8d62802ae0b1d4017ad0ac145b623903
yutaro-j31 <j31.ebc@icloud.com>
2026-07-17 07:00:20 +0900

    fix: preserve synchronous engine event timing

--- js/player-engine-bridge.js
+++ js/player-engine-bridge.js
@@ -6,30 +6,10 @@ if(!modules.engine?.TycoonEngine)throw new Error('engine.js must be loaded befor
 if(!modules.playerCrisisUI?.__installed)throw new Error('player-crisis-ui.js must be loaded before player-engine-bridge.js.');
 if(modules.playerEngineBridge)throw new Error('player engine bridge is already registered.');
 const EngineClass=modules.engine.TycoonEngine;
-const isWebKit=typeof navigator!=='undefined'&&/AppleWebKit/i.test(String(navigator.userAgent||''));
 let activeEngine=null;
-function installWebKitWeekYield(instance){
-  if(!instance||!isWebKit||instance.__webkitWeekYieldInstalled)return instance;
-  const baseEmit=instance.emit.bind(instance);
-  let timer=0,pendingDetail=null;
-  instance.emit=function(type='change',detail={}){
-    if(type!=='week')return baseEmit(type,detail);
-    pendingDetail=detail;
-    if(timer)return true;
-    timer=setTimeout(()=>{
-      timer=0;
-      const nextDetail=pendingDetail;
-      pendingDetail=null;
-      baseEmit('week',nextDetail||{});
-    },0);
-    return true;
-  };
-  Object.defineProperty(instance,'__webkitWeekYieldInstalled',{value:true});
-  return instance;
-}
-function bindEngine(instance){activeEngine=installWebKitWeekYield(instance||null);return instance;}
+function bindEngine(instance){activeEngine=instance||null;return instance;}
 function getEngine(){return activeEngine;}
 const baseLoad=EngineClass.load.bind(EngineClass);
 EngineClass.load=function(...args){return bindEngine(baseLoad(...args));};
-modules.playerEngineBridge=Object.freeze({bindEngine,getEngine,installWebKitWeekYield,__installed:true});
+modules.playerEngineBridge=Object.freeze({bindEngine,getEngine,__installed:true});
 })();
