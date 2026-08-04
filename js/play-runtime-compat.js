90ade01092a01026d6bc3896eae6c243a33151ff
yutaro-j31 <j31.ebc@icloud.com>
2026-08-05 03:23:00 +0900

    test: remove unproven final activism hook

--- js/play-runtime-compat.js
+++ js/play-runtime-compat.js
@@ -5,26 +5,8 @@ if(!modules?.engine?.TycoonEngine)return;
 if(!modules.playerDebtService&&modules.playerDebtRefinancing?.__installed){
   modules.playerDebtService=Object.freeze({__installed:true,compatibilityAlias:true});
 }
-const Engine=modules.engine.TycoonEngine;
-const activism=modules.shareholderActivism;
-if(activism?.processWeek&&!Engine.prototype.__shareholderActivismFinalWeeklyHook){
-  const baseAdvanceWeek=Engine.prototype.advanceWeek;
-  Engine.prototype.advanceWeek=function(){
-    const result=baseAdvanceWeek.apply(this,arguments);
-    if(result!==false){
-      const week=Math.max(0,Math.floor(Number(this.g?.week)||0));
-      if(this.g&&this.g.shareholderActivismLastProcessedWeek!==week){
-        activism.processWeek(this);
-        this.g.shareholderActivismLastProcessedWeek=week;
-      }
-    }
-    return result;
-  };
-  Object.defineProperty(Engine.prototype,'__shareholderActivismFinalWeeklyHook',{value:true});
-}
 modules.playRuntimeCompat=Object.freeze({
   debtServiceAliasReady:Boolean(modules.playerDebtService?.__installed),
-  shareholderActivismWeeklyHookReady:Boolean(activism?.processWeek&&Engine.prototype.__shareholderActivismFinalWeeklyHook),
   __installed:true
 });
 })();
