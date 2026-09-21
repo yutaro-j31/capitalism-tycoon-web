50d3ae1840b55a50f026383eedb8efc82634f60b
yutaro-j31 <j31.ebc@icloud.com>
2026-09-21 09:57:57 +0900

    test(pe): register network referral supply

--- tests/run-all.js
+++ tests/run-all.js
@@ -315,6 +315,7 @@ const nodeTests = [
   ['pe-fund-lp', 'tests/pe-fund-lp-test.js'],
   ['pe-lp-ddq-workflow', 'tests/pe-lp-ddq-workflow-test.js'],
   ['pe-fund-capital-gates-ui', 'tests/pe-fund-capital-gates-ui-test.js'],
+  ['pe-network-referral-supply', 'tests/pe-network-referral-supply-test.js'],
   ['pe-fund-team-slots', 'tests/pe-fund-team-slots-test.js'],
   ['pe-fund-dd-slots', 'tests/pe-fund-dd-slots-test.js'],
   ['pe-industry-tiers', 'tests/pe-industry-tiers-test.js'],
